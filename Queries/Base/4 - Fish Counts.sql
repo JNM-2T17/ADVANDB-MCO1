4ed9358f272fd3e5e9e69a29a85da755e8eb6f1f
Austin Fernandez <ryanaustinf@yahoo.com>
2016-02-25 21:48:49 +0800

    Documentation

--- Queries/Base/4 - Fish Counts.sql
+++ Queries/Base/4 - Fish Counts.sql
@@ -1,5 +1,5 @@
-SELECT H.mun,H.zone,H.brgy, aquanitype, COUNT(H.id) fishcount
+SELECT H.mun,H.zone,H.brgy, COUNT(H.id) fishcount
 FROM hpq_hh H, hpq_aquani A
 WHERE H.id = A.hpq_hh_id AND aquanitype = 6
-GROUP BY H.mun,H.zone,H.brgy,aquanitype
+GROUP BY H.mun,H.zone,H.brgy
 HAVING COUNT(H.id) > 0
