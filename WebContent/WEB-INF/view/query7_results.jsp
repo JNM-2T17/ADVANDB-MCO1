229b109b64e292151f86b2e29a7930d0240c508a
MDSP777 <mdsp777@outlook.com>
2016-02-25 19:56:25 +0800

    Added verbosity to front end pages

--- WebContent/WEB-INF/view/query7_results.jsp
+++ WebContent/WEB-INF/view/query7_results.jsp
@@ -1,6 +1,7 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
     pageEncoding="ISO-8859-1"%>
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ taglib prefix="fn" uri="http://java.sun.com/jsp/jstl/functions" %>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
@@ -8,6 +9,9 @@
 <title>Common Beneficiaries</title>
 </head>
 <body>
+	<h1>Common Beneficiaries</h1>
+	<h5>Total Row Count: ${fn:length(commonbeneficiaries) }</h5>
+	<h5>Query Execution Time: ${time }</h5>
 	<table>
 		<tr>
 			<th>Municipality</th>
--- src/web/TheController.java
+++ src/web/TheController.java
@@ -6,6 +6,7 @@ import java.util.Collection;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.swing.JOptionPane;
 
 import model.AvgDeathAge;
 import model.AvgOFWsPerNuclearFamily;
@@ -20,6 +21,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import dao.BaseQueries;
+import dao.OptimizedQueries;
 
 @Controller
 public class TheController {
@@ -29,55 +31,137 @@ public class TheController {
 	}
 	
 	@RequestMapping("/BaseQuery1")
-	public void baseQuery1(@RequestParam(value="val") int val, HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {	
-		Collection<AvgOFWsPerNuclearFamily> list = BaseQueries.getAvgOFWsPerNuclearFamilyWithOFWCountGreaterThan(val);
+	public void baseQuery1(@RequestParam(value="val") int val, @RequestParam(value="type") int type, 
+			HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {	
+		Collection<AvgOFWsPerNuclearFamily> list = null;
+		long time = System.currentTimeMillis();
+		switch(type){
+		case 1:
+			list = BaseQueries.getAvgOFWsPerNuclearFamilyWithOFWCountGreaterThan(val);
+			break;
+		case 2:
+			list = OptimizedQueries.getAvgOFWsPerNuclearFamilyWithOFWCountGreaterThan(val);
+			break;
+		}
+		time = System.currentTimeMillis()-time;
 		request.setAttribute("avgofws", list);
+		System.out.println(time);
+		request.setAttribute("time", time);
 		request.getRequestDispatcher("WEB-INF/view/query1_results.jsp").forward(request, response);
 	}
 	
 	@RequestMapping("/BaseQuery2")
-	public void baseQuery2(@RequestParam(value="val") int val, @RequestParam(value="minNutIndex") int minNutIndex, 
+	public void baseQuery2(@RequestParam(value="val") int val, @RequestParam(value="type") int type, 
+			@RequestParam(value="minNutIndex") int minNutIndex, 
 			HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {	
-		Collection<HealthyKids> list = BaseQueries.getPlacesWithHealthyKidsGreaterThan(val, minNutIndex);
+		Collection<HealthyKids> list = null;
+		long time = System.currentTimeMillis();
+		switch(type){
+		case 1:
+			list = BaseQueries.getPlacesWithHealthyKidsGreaterThan(val, minNutIndex);
+			break;
+		case 2:
+			list = OptimizedQueries.getPlacesWithHealthyKidsGreaterThan(val, minNutIndex);
+			break;
+		}
+		time = System.currentTimeMillis()-time;
 		request.setAttribute("healthykids", list);
+		request.setAttribute("time", time);
 		request.getRequestDispatcher("WEB-INF/view/query2_results.jsp").forward(request, response);
 	}
 	
 	@RequestMapping("/BaseQuery3")
-	public void baseQuery3(@RequestParam(value="val") int val, HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {	
-		Collection<AvgDeathAge> list = BaseQueries.getAvgDeathAgeGraterThan(val);
+	public void baseQuery3(@RequestParam(value="val") int val, @RequestParam(value="type") int type, 
+			HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {	
+		Collection<AvgDeathAge> list = null;
+		long time = System.currentTimeMillis();
+		switch(type){
+		case 1:
+			list = BaseQueries.getAvgDeathAgeGreaterThan(val);
+			break;
+		case 2:
+			list = OptimizedQueries.getAvgDeathAgeGreaterThan(val);
+			break;
+		}
+		time = System.currentTimeMillis()-time;
 		request.setAttribute("avgdeaths", list);
+		request.setAttribute("time", time);
 		request.getRequestDispatcher("WEB-INF/view/query3_results.jsp").forward(request, response);
 	}
 	
 	@RequestMapping("/BaseQuery4")
-	public void baseQuery4(@RequestParam(value="val") int val, @RequestParam(value="aquanitype") int aquanitype, 
+	public void baseQuery4(@RequestParam(value="val") int val, @RequestParam(value="type") int type, 
+			@RequestParam(value="aquanitype") int aquanitype, 
 			HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {	
-		Collection<FishCount> list = BaseQueries.getFishCountsGreaterThan(val, aquanitype);
+		Collection<FishCount> list = null;
+		long time = System.currentTimeMillis();
+		switch(type){
+		case 1:
+			list = BaseQueries.getFishCountsGreaterThan(val, aquanitype);
+			break;
+		case 2:
+			list = OptimizedQueries.getFishCountsGreaterThan(val, aquanitype);
+			break;
+		}
 		request.setAttribute("fishcount", list);
+		request.setAttribute("time", time);
 		request.getRequestDispatcher("WEB-INF/view/query4_results.jsp").forward(request, response);
 	}
 	
 	@RequestMapping("/BaseQuery5")
-	public void baseQuery5(@RequestParam(value="val") double val, HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {	
-		Collection<CropVolume> list = BaseQueries.getCropVolumesGreaterThan(val);
+	public void baseQuery5(@RequestParam(value="val") double val, @RequestParam(value="type") int type, 
+			HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {	
+		Collection<CropVolume> list = null;
+		long time = System.currentTimeMillis();
+		switch(type){
+		case 1:
+			list = BaseQueries.getCropVolumesGreaterThan(val);
+			break;
+		case 2:
+			list = OptimizedQueries.getCropVolumesGreaterThan(val);
+			break;
+		}
 		request.setAttribute("cropvolume", list);
+		request.setAttribute("time", time);
 		request.getRequestDispatcher("WEB-INF/view/query5_results.jsp").forward(request, response);
 	}
 	
 	@RequestMapping("/BaseQuery6")
-	public void baseQuery6(@RequestParam(value="val") double val, 
+	public void baseQuery6(@RequestParam(value="val") double val, @RequestParam(value="type") int type,  
 			@RequestParam(value="aquaequiptype") int aquaequiptype, @RequestParam(value="aquanitype") int aquanitype, 
 			HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {	
-		Collection<CatchRatio> list = BaseQueries.getCatchRatiosGreaterThan(val, aquaequiptype, aquanitype);
+		Collection<CatchRatio> list = null;
+		long time = System.currentTimeMillis();
+		switch(type){
+		case 1:
+			list = BaseQueries.getCatchRatiosGreaterThan(val, aquaequiptype, aquanitype);
+			break;
+		case 2:
+			list = OptimizedQueries.getCatchRatiosGreaterThan(val, aquaequiptype, aquanitype);
+			break;
+		}
 		request.setAttribute("catchratios", list);
+		request.setAttribute("time", time);
 		request.getRequestDispatcher("WEB-INF/view/query6_results.jsp").forward(request, response);
 	}
 	
 	@RequestMapping("/BaseQuery7")
-	public void baseQuery7(@RequestParam(value="val") int val, HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {	
-		Collection<CommonBeneficiary> list = BaseQueries.getCommonBeneficiariesGraterThan(val);
+	public void baseQuery7(@RequestParam(value="val") int val, @RequestParam(value="type") int type, 
+			HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("HELLO SHIT");
+		Collection<CommonBeneficiary> list = null;
+		long time = System.currentTimeMillis();
+		switch(type){
+		case 1:
+			list = BaseQueries.getCommonBeneficiariesGreaterThan(val);
+			break;
+		case 2:
+			list = OptimizedQueries.getCommonBeneficiariesGreaterThan(val);
+			break;
+		}
+		JOptionPane.showMessageDialog(null, time);
 		request.setAttribute("commonbeneficiaries", list);
+		request.setAttribute("time", time);
 		request.getRequestDispatcher("WEB-INF/view/query7_results.jsp").forward(request, response);
 	}
 }
