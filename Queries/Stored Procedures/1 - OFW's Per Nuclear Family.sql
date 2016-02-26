80d32107e0f0b4c1adbdb9e5ec0188c8489587a9
Austin Fernandez <ryanaustinf@yahoo.com>
2016-02-26 15:19:33 +0800

    Updates

--- Queries/Stored Procedures/1 - OFW's Per Nuclear Family.sql
+++ Queries/Stored Procedures/1 - OFW's Per Nuclear Family.sql
@@ -1,15 +1,18 @@
 CREATE INDEX HIndex
 ON db_hpq.hpq_hh (nnucfam);
 
+CREATE OR REPLACE VIEW hhnucfam AS
+SELECT mun, zone, brgy, purok, nnucfam, nofw
+		FROM db_hpq.hpq_hh
+		WHERE nnucfam > 0;
+
 DELIMITER $$
 CREATE PROCEDURE query1(IN minCount INT)
 BEGIN
 SELECT mun, zone, brgy, purok
 		, SUM(nnucfam) AS `Nuclear Families`, SUM(nofw) AS OFWs
         , SUM(nofw) / SUM(nnucfam) AS `Average OFW's per Nuclear Family`
-FROM (SELECT mun, zone, brgy, purok, nnucfam, nofw
-		FROM db_hpq.hpq_hh
-		WHERE nnucfam > 0) A
+FROM hhnucfam
 GROUP BY mun, zone, brgy, purok
 HAVING SUM(nofw) > minCount;
 END$$
@@ -17,4 +20,5 @@ END$$
 DELIMITER ;
 
 CALL query1(0);
--- ALTER TABLE hpq_hh DROP INDEX HIndex;
+
+ALTER TABLE hpq_hh DROP INDEX HIndex;
--- Queries/Views/1 - OFW's Per Nuclear Family.sql
+++ Queries/Views/1 - OFW's Per Nuclear Family.sql
@@ -1,12 +1,15 @@
 CREATE INDEX HIndex
 ON db_hpq.hpq_hh (nnucfam);
 
+CREATE OR REPLACE VIEW hhnucfam AS
+SELECT mun, zone, brgy, purok, nnucfam, nofw
+		FROM db_hpq.hpq_hh
+		WHERE nnucfam > 0;
+
 SELECT mun, zone, brgy, purok
 		, SUM(nnucfam) AS `Nuclear Families`, SUM(nofw) AS OFWs
         , SUM(nofw) / SUM(nnucfam) AS `Average OFW's per Nuclear Family`
-FROM (SELECT mun, zone, brgy, purok, nnucfam, nofw
-		FROM db_hpq.hpq_hh
-		WHERE nnucfam > 0) A
+FROM hhnucfam
 GROUP BY mun, zone, brgy, purok
 HAVING SUM(nofw) > 0;
 
