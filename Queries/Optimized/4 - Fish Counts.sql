3321adff7fdc579b24ef444c90f9b6c755ec3a86
Austin Fernandez <ryanaustinf@yahoo.com>
2016-02-26 13:08:16 +0800

    Revised fishcounts

--- Queries/Optimized/4 - Fish Counts.sql
+++ Queries/Optimized/4 - Fish Counts.sql
@@ -3,7 +3,7 @@ FROM (SELECT id,mun,zone,brgy
 		FROM hpq_hh) H INNER JOIN 
 	(SELECT hpq_hh_id, aquanitype 
 		FROM hpq_aquani
-        WHERE aquanitype = 6) A
+        WHERE aquanitype = 2) A
 	ON H.id = A.hpq_hh_id
 GROUP BY H.mun,H.zone,H.brgy
 HAVING COUNT(H.id) > 0
